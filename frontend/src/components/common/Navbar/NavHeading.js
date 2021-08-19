7eb3e66c929f319f98a99206d7ed2abf4d335985
Madi-S <khovansky99@gmail.com>
2021-08-19 16:33:34 +0600

    Nav wrappers added and styled components installed

---
+++ frontend/src/components/common/Navbar/NavHeading.js
@@ -0,0 +1,5 @@
+const NavHeading = ({ text }) => {
+    return <h1 className='nav__header'>{text}</h1>
+}
+
+export default NavHeading
--- frontend/src/components/common/Navbar/Navbar.js
+++ frontend/src/components/common/Navbar/Navbar.js
@@ -1,21 +1,33 @@
 import NavLink from './NavLink'
-import './index.scss'
 import NavLogo from './NavLogo'
+import NavHeading from './NavHeading'
+import './index.scss'
+import styled from 'styled-components'
+
+const NavLogoWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const NavLinksWrapper = styled.div`
+
+`
 
 const Navbar = () => {
     return (
         <nav className='nav' data-testid='navbar'>
-            <div className='nav__logo-wrapper'>
+            <NavLogoWrapper>
                 <NavLogo />
-            <h1 className='nav__header'>Madi-S</h1>
-            </div>
-            <div className='nav__links-wrapper'>
+                <NavHeading text='Madi-S' />
+            </NavLogoWrapper>
+
+            <NavLinksWrapper>
                 <NavLink text='About me' to='about-me' />
                 <NavLink text='Feedback' to='feedback' />
                 <NavLink text='Courses' to='courses' />
                 <NavLink text='Login' to='login' />
                 <NavLink text='Register' to='register' />
-            </div>
+            </NavLinksWrapper>
         </nav>
     )
 }
