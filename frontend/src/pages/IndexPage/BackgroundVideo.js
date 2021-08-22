fe7d9afd072b449931fd7cfdf22527e6fb4cfec3
Madi-S <khovansky99@gmail.com>
2021-08-22 10:01:05 +0600

    BackgroundVideo display fixed

--- frontend/src/pages/IndexPage/BackgroundVideo.js
+++ frontend/src/pages/IndexPage/BackgroundVideo.js
@@ -1,52 +1,39 @@
 import styled from 'styled-components'
 import Video from './../../misc/video/coding.mp4'
 
-const WRAPPER_HEIGHT_PX = 600
-const CONTENT_WRAPPER_TOP_PX = WRAPPER_HEIGHT_PX - 370
-
 const BackgroundVideo = props => {
-    const videoStyles = {
-        minWidth: '100%',
-        minHeight: '100%',
-        zIndex: -1,
-        filter: 'brightness(45%)',
-    }
-
     return (
         <Wrapper>
-            <VideoWrapper>
-                <video
-                    style={videoStyles}
-                    autoPlay='autoplay'
-                    loop='loop'
-                    muted
-                >
-                    <source src={Video} type='video/mp4' />
-                    Your browser does not support the video tag.
-                </video>
-            </VideoWrapper>
+            <video
+                style={{
+                    width: '100vw',
+                    height: '85vh',
+                    margin: '0',
+                    padding: '0',
+                    display: 'block'
+                }}
+                autoPlay='autoplay'
+                loop='loop'
+                muted
+            >
+                <source src={Video} type='video/mp4' />
+                Your browser does not support the video tag.
+            </video>
             <ContentWrapper>{props.children}</ContentWrapper>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    width: 100%;
-    height: ${WRAPPER_HEIGHT_PX}px;
-    
-`
-
-const VideoWrapper = styled.div`
-    z-index: -999;
-    width: 100%;
-    height: 100%;
+    width: 100vh;
+    height: 85vh;
 `
 
 const ContentWrapper = styled.div`
     position: absolute;
     left: 0;
     right: 0;
-    top: ${CONTENT_WRAPPER_TOP_PX}px;
+    top: 570px;
     text-align: center;
     display: flex;
     align-items: center;
